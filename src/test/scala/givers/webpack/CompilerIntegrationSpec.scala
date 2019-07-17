f866b718c739ab93716384c024856bf36a43b11d
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2019-07-18 05:06:50 +0700

    Add test for tracking CSS files (#17)

--- src/test/scala/givers/webpack/CompilerIntegrationSpec.scala
+++ src/test/scala/givers/webpack/CompilerIntegrationSpec.scala
@@ -28,10 +28,11 @@ object CompilerIntegrationSpec extends BaseSpec {
           nodeModules =  new File("node_modules")
         )
 
-        val baseInputDir = new File("src") / "test" / "scala" / "givers" / "webpack" / "assets" / "javascripts"
-        val aJs = baseInputDir / "a.js"
-        val bJs = baseInputDir / "b.js"
-        val cJs = baseInputDir / "common" / "c.js"
+        val baseInputStylesheetDir = new File("src") / "test" / "scala" / "givers" / "webpack" / "assets" / "stylesheets"
+        val baseInputJsDir = new File("src") / "test" / "scala" / "givers" / "webpack" / "assets" / "javascripts"
+        val aJs = baseInputJsDir / "a.js"
+        val bJs = baseInputJsDir / "b.js"
+        val cJs = baseInputJsDir / "common" / "c.js"
 
         val output1 = targetDir / "dist" / "compiled.js"
         val output2 = targetDir / "dist" / "another-compiled.js"
@@ -44,38 +45,59 @@ object CompilerIntegrationSpec extends BaseSpec {
         val result = compiler.compile(entries, inputs.map(_.toPath))
 
         result.success ==> true
-        result.entries.size ==> 4
+        result.entries.size ==> 8
 
-        result.entries.head.inputFile.getCanonicalPath ==> aJs.getCanonicalPath
-        result.entries.head.filesWritten.size ==> 2
-        Files.exists(result.entries.head.filesWritten.head) ==> true
-        result.entries.head.filesWritten ==> Set(output1.toPath, output1.toPath.getParent.resolve(s"${output1.name}.map"))
-        // If CSS dependency is tracked properly, the below should have been true.
-        // See more: https://github.com/GIVESocialMovement/sbt-vuefy/issues/20
-        //        result.entries.head.filesRead ==> inputs.map(_.toPath).toSet ++ Set(baseInputDir / "dependencies" / "style.scss")
+        // api.js is a file in css-loader and involved in generating compiled.js.
+        // It makes sense to let sbt-web track the file.
+        // This means, if css-loader is updated, compiled.js will be re-compiled, which is amazing!
+        result.entries.head.inputFile.getCanonicalPath ==> (new File(".") / "node_modules" / "css-loader" / "dist" / "runtime" / "api.js").getCanonicalPath
+        result.entries.head.filesWritten ==> Set(
+          output1.toPath,
+          output1.toPath.getParent.resolve(s"${output1.name}.map")
+        )
 
-        result.entries(1).inputFile.getCanonicalPath ==> bJs.getCanonicalPath
-        result.entries(1).filesWritten.size ==> 4
+        result.entries(1).inputFile.getCanonicalPath ==> aJs.getCanonicalPath
         result.entries(1).filesWritten.forall { f => Files.exists(f) } ==> true
-        result.entries(1).filesWritten ==> Set(
+        result.entries(1).filesWritten ==> Set(output1.toPath, output1.toPath.getParent.resolve(s"${output1.name}.map"))
+
+        result.entries(2).inputFile.getCanonicalPath ==> bJs.getCanonicalPath
+        result.entries(2).filesWritten.forall { f => Files.exists(f) } ==> true
+        result.entries(2).filesWritten ==> Set(
           output1.toPath,
           output1.toPath.getParent.resolve(s"${output1.name}.map"),
           output2.toPath,
           output2.toPath.getParent.resolve(s"${output2.name}.map"),
         )
 
-        result.entries(2).inputFile.getCanonicalPath ==> cJs.getCanonicalPath
-        result.entries(2).filesWritten.size ==> 2
-        result.entries(2).filesWritten.forall { f => Files.exists(f) } ==> true
-        result.entries(2).filesWritten ==> Set(output1.toPath, output1.toPath.getParent.resolve(s"${output1.name}.map"))
+        result.entries(3).inputFile.getCanonicalPath ==> cJs.getCanonicalPath
+        result.entries(3).filesWritten.forall { f => Files.exists(f) } ==> true
+        result.entries(3).filesWritten ==> Set(output1.toPath, output1.toPath.getParent.resolve(s"${output1.name}.map"))
 
         // The below is the code-splitting defined in assets/webpack.config.js
-        result.entries(3).inputFile.getCanonicalPath ==> (baseInputDir / "vendor" / "lib.js").getCanonicalPath
-        result.entries(3).filesWritten.size ==> 2
-        result.entries(3).filesWritten.forall { f => Files.exists(f) } ==> true
-        result.entries(3).filesWritten ==> Set(
+        result.entries(4).inputFile.getCanonicalPath ==> (baseInputJsDir / "vendor" / "lib.js").getCanonicalPath
+        result.entries(4).filesWritten.size ==> 2
+        result.entries(4).filesWritten.forall { f => Files.exists(f) } ==> true
+        result.entries(4).filesWritten ==> Set(
           (targetDir / "dist" / "vendor.js").toPath,
           (targetDir / "dist" / "vendor.js.map").toPath,
+        )
+
+        result.entries(5).inputFile.getCanonicalPath ==> (baseInputStylesheetDir / "dep.scss").getCanonicalPath
+        result.entries(5).filesWritten ==> Set(
+          output1.toPath,
+          output1.toPath.getParent.resolve(s"${output1.name}.map"),
+        )
+
+        result.entries(6).inputFile.getCanonicalPath ==> (baseInputStylesheetDir / "sass.scss").getCanonicalPath
+        result.entries(6).filesWritten ==> Set(
+          output1.toPath,
+          output1.toPath.getParent.resolve(s"${output1.name}.map"),
+        )
+
+        result.entries(7).inputFile.getCanonicalPath ==> (baseInputStylesheetDir / "test.css").getCanonicalPath
+        result.entries(7).filesWritten ==> Set(
+          output1.toPath,
+          output1.toPath.getParent.resolve(s"${output1.name}.map"),
         )
       }
     }
--- src/test/scala/givers/webpack/assets/webpack.config.js
+++ src/test/scala/givers/webpack/assets/webpack.config.js
@@ -2,6 +2,18 @@
 
 module.exports = {
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      },
+      {
+        test: /\.scss$/i,
+        use: ['css-loader', 'sass-loader'],
+      },
+    ],
+  },
   optimization: {
     splitChunks: {
       cacheGroups: {
